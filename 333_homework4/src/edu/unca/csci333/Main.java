31cdec5bdc10476f10779d5bc35110950fc2218a
Josie <jandersoj03@gmail.com>
2024-09-30 18:28:23 -0400

    created classes for homework4

---
+++ 333_homework4/src/edu/unca/csci333/Main.java
@@ -0,0 +1,28 @@
+package edu.unca.csci333;
+
+public class Main {
+//	Define a private helper method void countingSort.
+//	Implement Counting Sort as in the slides/textbook. 
+//	One parameter will be the input int [ ] to sort. 
+//	Another parameter is for another array where the output is stored. 
+//	The third parameter is for the largest single input value from the input array. 
+//	Make your Java code zero-indexed,
+//	but be aware the pseudocode one-indexes A and B, while zero-indexing the auxiliary array C.
+	
+//	Define a public method int [ ] countingSort(int [ ] ), 
+//	with a single parameter of type int [ ] and a return type of int [ ] as well. 
+//	This wrapper method will take as input an int array, and return a sorted copy. 
+//	so, in this wrapper, calculate the value of argument k, memory allocate an empty array, 
+//	then invoke the private countingSort. 
+//	(The private counting sort method should fill the empty array with a sorted copy of the input array.)
+	
+//	main method to test counting sort of several different arrays, and also test randomized quickselect several times. 
+//	To test quickselect, you'll need to declare and construct an object of type Quickselect, 
+//	then invoke its public randomizedQuickselect method.
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 333_homework4/src/edu/unca/csci333/Quickselect.java
@@ -0,0 +1,24 @@
+package edu.unca.csci333;
+import java.util.*;
+
+public class Quickselect {
+//	Define a constructor that takes a T [ ] param handle 
+//	and initializes a data field with a shallow copy from copying the array handle.
+	
+//	Define the private helper method partition, reused from the Quicksort assignment.
+	
+//	Define a private helper method T randomizedQuickselect. 
+//	Implement Randomized Quickselect as in the slides/textbook. 
+//	One parameter is an array of type T [ ].  
+//	Two more parameters are the inclusive bounds of the subarray to consider.
+//	One more parameter is i, the order statistic to find in the parameter array. 
+//	You are searching in the parameter array, not the data field array.
+	
+//	Define a public wrapper method T randomizedQuickselect, with only one parameter: the desired order statistic i.  
+//	This public wrapper method will ensure that the select will occur nondestructively. 
+//	Make a deep copy of the data field array to create another T [ ] of the same length filled with the same handles. 
+//	Then, make the top level call of private randomizedQuickselect with the copied array. 
+//	(As the private method rearranges this copy, the original data field array remains pristine.)
+	
+
+}
