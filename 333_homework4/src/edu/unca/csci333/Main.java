74395d3db523db1cfee473bd0a30ff19c8ccf361
Josie <jandersoj03@gmail.com>
2024-11-07 23:18:23 -0500

    Homework4

--- 333_homework4/src/edu/unca/csci333/Main.java
+++ 333_homework4/src/edu/unca/csci333/Main.java
@@ -1,36 +1,150 @@
 package edu.unca.csci333;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
-	
-//	Define a private helper method void countingSort.
-	private void countingSort(int[] toSort, int[] arrOut, int largestInput) {
-//	Implement Counting Sort as in the slides/textbook. 
-//	One parameter will be the input int [ ] to sort. 
-//	Another parameter is for another array where the output is stored. 
-//	The third parameter is for the largest single input value from the input array. 
-		
-//	Make your Java code zero-indexed,
-//	but be aware the pseudocode one-indexes A and B, while zero-indexing the auxiliary array C.
-	}
-		
-//	Define a public method int [ ] countingSort(int [ ] ), 
-	public int[] countingSort(int[] arrIn) {
-//	with a single parameter of type int [ ] and a return type of int [ ] as well. 
-//	This wrapper method will take as input an int array, and return a sorted copy. 
-//	so, in this wrapper, calculate the value of argument k, memory allocate an empty array, 
-//	then invoke the private countingSort. 
-//	(The private counting sort method should fill the empty array with a sorted copy of the input array.)
-		return arrIn;
-	}
 			
-	
-//	main method to test counting sort of several different arrays, and also test randomized quickselect several times. 
-//	To test quickselect, you'll need to declare and construct an object of type Quickselect, 
-//	then invoke its public randomizedQuickselect method.
-	
+	/**
+	 * Main method for testing Quickselect and countingSort
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Random random = new Random();		
+//	Quickselect tests
+		System.out.println("Quickselect tests:");
+		Integer[] arr1 = randIntegerArray(0, 250);
+		Quickselect<Integer> test1 = new Quickselect<Integer>(arr1);
+		runTestsQS(test1, arr1.length);
+		
+		Integer[] arr2 = randIntegerArray(0, 250);
+		Quickselect<Integer> test2 = new Quickselect<Integer>(arr2);
+		runTestsQS(test2, arr2.length);
+		
+		Character[] arr3 = randCharacterArray();
+		Quickselect<Character> test3 = new Quickselect<Character>(arr3);
+		runTestsQS(test3, arr3.length);
+		
+		Character[] arr4 = randCharacterArray();
+		Quickselect<Character> test4 = new Quickselect<Character>(arr4);
+		runTestsQS(test4, arr4.length);
+		
+//	countingSort tests:
+		System.out.println("countingSort tests:");
+		System.out.println();
+		int[] cs1 = randIntArray(0, 250);
+		System.out.println("unsorted: "+Arrays.toString(cs1));
+		System.out.println("sorted: "+Arrays.toString(countingSort(cs1)));
+		
+		System.out.println();
+		int[] cs2 = randIntArray(0, 250);
+		System.out.println("unsorted: "+Arrays.toString(cs2));
+		System.out.println("sorted: "+Arrays.toString(countingSort(cs2)));
+		
+		System.out.println();
+		int[] cs3 = randIntArray(0, 250);
+		System.out.println("unsorted: "+Arrays.toString(cs3));
+		System.out.println("sorted: "+Arrays.toString(countingSort(cs3)));
+	}	
+	
+	/**
+	 * Private function to sort an array of int[] from smallest to largest
+	 * @param toSort Copied array to be sorted
+	 * @param arrOut Sorted array output
+	 * @param largestInput The maximum number in the input array. 
+	 */
+	private static void countingSort(int[] toSort, int[] arrOut, int largestInput) {
 
+		int[] counter = new int[largestInput+1];
+
+		for(int i = 0; i < toSort.length; i++){
+			counter[toSort[i]]++;
+		}
+
+		for(int i = 1; i <= largestInput; i++) {
+			counter[i] += counter[i-1];
+		}
+		
+		for(int i = toSort.length-1; i >=0 ; i--) {
+			arrOut[counter[toSort[i]]-1] = toSort[i];
+			counter[toSort[i]]--;
+		}
 	}
-
+		
+	/**
+	 * Public wrapper for the countingSort method, creates a deep copy of the array to be sorted.
+	 * @param arrIn Array to be copied and sorted
+	 * @return Sorted array
+	 */
+	public static int[] countingSort(int[] arrIn) {
+		int max = arrIn[0];
+		for(int i = 0; i < arrIn.length; i++) {
+			if(arrIn[i] > max) max = arrIn[i];
+		}
+		
+		int[] arrOut = new int[arrIn.length];
+		
+		countingSort(arrIn, arrOut, max);
+		return arrOut;
+	}
+	
+	/**
+	 * Public printing method
+	 * @param <T> Type parameter to work with various types
+	 * @param arrIn The array to be printed
+	 */
+	public static <T> void printArr(T[] arrIn) {
+		String str = "[";
+		for(int i = 0; i < arrIn.length-1; i++) {
+			str += arrIn[i] + ", ";
+		}
+		str += arrIn[arrIn.length-1] + "]";
+		System.out.println(str);
+	}
+	
+	
+	/**
+	 *Various private methods for testing:
+	 */
+	private static void runTestsQS(Quickselect<?> in, int len){
+		System.out.println("Unsorted array: " + in.toString());
+		System.out.println("1st order stat.: " + in.randomizedQuickselect(1));
+		System.out.println("10th order stat.: " + in.randomizedQuickselect(10));
+		System.out.println("Last order stat.: " + in.randomizedQuickselect(len));
+		System.out.println("Median: " + in.randomizedQuickselect((len+1)/2));
+		System.out.println();
+		System.out.println("------------------------------");
+	}
+	
+ 	private static Integer[] randIntegerArray(int min, int max) {
+		Random r = new Random();		
+		int length = r.nextInt(19)+10;
+		Integer[] arr = new Integer[length];
+		for(int i = 0; i < length; i++) {
+			arr[i] = Integer.valueOf(r.nextInt(max - min + 1) + min);
+		}
+		return arr;
+	}
+	
+	private static int[] randIntArray(int min, int max) {
+		Random r = new Random();	
+		int length = r.nextInt(19)+10;
+		int[] arr = new int[length];
+		for(int i = 0; i < length; i++) {
+			arr[i] = r.nextInt(max - min + 1) + max;
+		}
+		return arr;
+	}
+	
+	private static Character[] randCharacterArray(){
+		Random r = new Random();
+		int length = r.nextInt(19)+10;
+		Character[] arr = new Character[length];
+		char c;
+		for(int i = 0; i < length; i++) {
+			c = (char)(r.nextInt(52)+'A'); //note that capital letters are sorted to the left of lowercase
+			arr[i] = Character.valueOf(c);
+		}
+		return arr;
+	}
 }
--- 333_homework4/src/edu/unca/csci333/Quickselect.java
+++ 333_homework4/src/edu/unca/csci333/Quickselect.java
@@ -5,16 +5,15 @@ public class Quickselect<T extends Comparable<T>> {
 	
 	private T[] arr;
 	private Random random;
-
-	
+	/**
+	 * Constructor method for a Quickselect object
+	 * @param arrIn Input for the Quickselect's array
+	 */
 	Quickselect(T[] arrIn){
-//	Define a constructor that takes a T [ ] param handle 
-//	and initializes a data field with a shallow copy from copying the array handle.
 		this.arr = arrIn;
 		this.random = new Random();		
 	}
-	
-//	Define the private helper method partition, reused from the Quicksort assignment.
+
 	/**
 	 * Public method rearranging Quickselect arrays and subproblems so that:
 	 * All elements less than or equal to the element at the end index are placed before it.
@@ -24,64 +23,83 @@ public class Quickselect<T extends Comparable<T>> {
 	 * @param end The index to select the end of the partitioning interval.
 	 * @return The index of the pivot element after being partitioned.
 	 */
-	private int partition(int start, int end) {
-		T pivot = arr[end];
+	private int partition(T[] arrIn, int start, int end) {
+		T pivot = arrIn[end];
 		int i = start-1;
 		
 		for(int j = start; j < end; j++) { 
-		   if(arr[j].compareTo(pivot) <= 0) { // if the value is less than the pivot
+		   if(arrIn[j].compareTo(pivot) <= 0) { // if the value is less than the pivot
 		      i++;
-		      swap(i, j); //it gets swapped left
+		      if(i != j)swap(arrIn, i, j); //it gets swapped left
 		   }
 		}
-		swap(i+1, end); // put pivot between left and right sides
+		swap(arrIn, i+1, end); // put pivot between left and right sides
 		return i+1; //returns the pivot point as q for quicksort.
 	}
 	
 	
-//	Define a private helper method T randomizedQuickselect. 
-//	Implement Randomized Quickselect as in the slides/textbook. 
-//	One parameter is an array of type T [ ].  
-//	Two more parameters are the inclusive bounds of the subarray to consider.
-//	One more parameter is i, the order statistic to find in the parameter array. 
-//	You are searching in the parameter array, not the data field array.
-	private T randomizedQuickselect(int start, int end, int i) {
-		if(start == end) // base case is length-1 subarray
-			return arr[start];
-		int rand = random.nextInt(end - start + 1) + start;// random int between p and r inclusive
-		swap(rand, end);
-		int q = partition(start, end); // the same PARTITION used in quicksort
+	/**
+	 * Private recursive randomized Quickselect method to locate and select a given order statistic.
+	 * @param arrIn The array to be searched/selected from
+	 * @param start The beginning of the array
+	 * @param end The end of the array
+	 * @param i The order statistic to be selected
+	 * @return Either returns the selected value or recursively calls into subarrays.
+	 */
+	private T randomizedQuickselect(T[] arrIn, int start, int end, int i) {
+		if(start == end)
+			return arrIn[start];
+		
+		int rand = random.nextInt(end - start + 1) + start;
+		swap(arrIn, rand, end);
+		
+		int q = partition(arrIn, start, end);
 		int k = q - start + 1; // calculate the local order statistic k of the pivot
-		if(i == k) // check if pivot is the ith order statistic, our answer!
-		   return arr[q];
-		else if (i < rand) // we’re looking for an earlier order statistic
-			return randomizedQuickselect(start, q-1, i);
-		else return randomizedQuickselect(q+1, end, i-k);
+		
+		if(i == k)
+		   return arrIn[q];
+		else if (i < k)
+			return randomizedQuickselect(arrIn, start, q-1, i);
+		else return randomizedQuickselect(arrIn, q+1, end, i-k);
 	}
 	
-//	Define a public wrapper method T randomizedQuickselect, with only one parameter: the desired order statistic i.  
-//	This public wrapper method will ensure that the select will occur nondestructively. 
-//	Make a deep copy of the data field array to create another T [ ] of the same length filled with the same handles. 
-//	Then, make the top level call of private randomizedQuickselect with the copied array. 
-//	(As the private method rearranges this copy, the original data field array remains pristine.)
+	/**
+	 * Public wrapper method for randomizedQuickelect, passes in a new deep copy of source array.
+	 * @param i the target order statistic
+	 * @return a call to the recursive function randomizedQuickselect with the copied array
+	 */
 	public T randomizedQuickselect(int i) {
-		T newArr[] = new T[arr.length];
-		for(int j = 0; j < arr.length; j++) {
+		T newArr[] = (T[])new Comparable[arr.length];
+		int j;
+		for(j = 0; j < arr.length; j++) {
 			newArr[j]  = arr[j];
 		}
-		return arr[0];
+		return randomizedQuickselect(newArr, 0, arr.length-1, i); 
 	}
 	
+	/**
+	 * Public method to return a string representation of the array
+	 * @return a string representation of the array
+	 */
+	public String toString() {
+		String str = "[";
+		for(int i = 0; i < arr.length; i++) {
+			str += arr[i] + ", ";
+		}
+		str += arr[arr.length-1] + "]";
+		return str;
+	}
+		
 	/**
 	 * Private method to swap the values at two indices
 	 * 
 	 * @param index1 the first index
 	 * @param index2 the second index
 	 */
-	private void swap(int index1, int index2) {
-		T temp = arr[index1];
-		arr[index1] = arr[index2];
-		arr[index2] = temp;
+	private void swap(T[] arrIn, int index1, int index2) {
+		T temp = arrIn[index1];
+		arrIn[index1] = arrIn[index2];
+		arrIn[index2] = temp;
 	}
 
 }
