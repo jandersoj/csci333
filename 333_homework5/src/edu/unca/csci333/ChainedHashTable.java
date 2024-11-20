980d9a9245cfe5322b334a6fa8d1fff89a072d9a
Josie <jandersoj03@gmail.com>
2024-11-19 19:08:22 -0500

    more of hw5

--- 333_homework5/src/edu/unca/csci333/ChainedHashTable.java
+++ 333_homework5/src/edu/unca/csci333/ChainedHashTable.java
@@ -4,7 +4,9 @@ import java.util.*;
 public class ChainedHashTable<T> {
 //	Implement a Java class named ChainedHashTable, based on a hash table with chaining using the multiplication method from the slides/textbook. 
 //	Your should use an array of LinkedList<T> as your primary instance variable / data field.
-	LinkedList<T>[] data;
+	private LinkedList<T>[] table;
+	private double A;
+	private int m;
 	
 //	The constructor should have a parameter for n, the total number of inputs expected. 
 	@SuppressWarnings("unchecked")
@@ -12,28 +14,50 @@ public class ChainedHashTable<T> {
 //	In the constructor, create the array of size m as the first power of 2 larger than n, 
 //	full of constructed but empty linked lists at each index. 
 
-		int m = nextLargestPowOf2(n);
-		data = new LinkedList[m];
+		m = nextLargestPowOf2(n);
+		table = new LinkedList[m];
 		for(int i = 0; i < m; i++) {
-			data[i] = new LinkedList<T>();
+			table[i] = new LinkedList<T>();
 		}
 	}
 	
 //	There should also be public methods for insert, delete, and search as in the slides/textbook.
-	public void insert(T key) {
-		int index = hash(key);
-		data[index].add(key);
+	public void insert(T element) {
+		int index = hash(element.hashCode()); //so this is taking the key (item.hashCode()) and hashing that to calculate the index
+		table[index].addFirst(element); //and then at that index it is inserting the element itself, NOT the key.
+	}	//perfect.
+	
+	public void delete(T element) {
+		int index = hash(element.hashCode());
+		table[index].remove(element);
 	}
+	
+//	for each index i, T[i] is a handle to the element of key i.
+	public LinkedList<T> search(int key) {
+		int targetIndex = hash(key);
+		System.out.println(key+" hashed to "+targetIndex);
+		if(table[targetIndex] != null) return table[targetIndex];
+		return null;		
+	}
+	
 //	Define a toString method for the entire hash table, which runs a loop and 
 //	concatenates each linked list in the hash table into the returned String; 
-//	consider using the LinkedList class's toString method.s
+//	consider using the LinkedList class's toString method.
+	public String toString() {
+		String out = "";
+		for(int i = 0; i < m; i++) {
+			out += table[i].toString();
+		}
+		return out;
+	}
 	
 	
 //	You will need a private helper method named hash. 
 //	Given an int key parameter, it computes and returns an int index of the table.
 	private int hash(int key) {
-		double A = 0.6180339887;
-		return (int) Math.floor(data.length*(key*A%1));
+		A = 0.6180339887;
+		System.out.println("hashed "+key+" to "+ (int)(m*((key*A)%1)));
+		return (int)(m*((key*A)%1)); //i THINK this is right
 	}
 	
 //  5 points extra credit: Use a Universal collection of hash functions instead of the multiplication method, for the ChainedHashTable class above. 
@@ -44,7 +68,7 @@ public class ChainedHashTable<T> {
 	private static int nextLargestPowOf2(int num) {
 		int next = 1; 
 		while (next < num)
-			next <<= 1;
+			next *= 2;
 		return next;
 	}
 }
--- 333_homework5/src/edu/unca/csci333/Main.java
+++ 333_homework5/src/edu/unca/csci333/Main.java
@@ -1,11 +1,43 @@
 package edu.unca.csci333;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ChainedHashTable test = new ChainedHashTable(5);
 		
+		System.out.println("ChainedHashTable tests:");
+	//create table:
+		ChainedHashTable<Integer> test1 = new ChainedHashTable(20);
+		int[] arr = {99, 62, 60, 17, 66, 70, 1, 11, 88, 81, 21, 60, 15, 69, 7, 61, 79, 93, 79, 70};
+		Integer[] values = intsToIntegers(arr);
+		System.out.println("Empty ChainedHashTable:");
+		System.out.println(test1.toString());
+	//fill table:
+		for(int i = 0; i < values.length; i++) {
+			test1.insert(values[i]);
+		}
+		System.out.println("ChainedHashTable after inserting 20 elements:");
+		System.out.println(test1.toString());
+	//delete from table:
+		test1.delete(values[4]);
+		test1.delete(values[7]);
+		test1.delete(values[12]);
+		System.out.println("ChainedHashTable after deleting 3 elements:");
+		System.out.println(test1.toString());
+	//search present keys:
+		System.out.println("Any element(s) with key "+values[6].hashCode() +" are in the LinkedList "+test1.search(values[6].hashCode()));
+		//I didn't return the index above because it said the "handle", and wouldn't that be the LinkedList itself?
+
+
+	}
+
+	//takes an array of ints and converts them to Integers. Secret for testing. I hate typing repetitive lines.
+	private static Integer[] intsToIntegers(int[] ints) {
+		Integer[] out = new Integer[ints.length];
+		for(int i = 0; i < ints.length; i++) {
+			out[i] = Integer.valueOf(ints[i]);
+		}
+		return out;	
 	}
 
 }
